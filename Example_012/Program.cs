40cfa7c9cec346b29955ad6bdff2f071b84113f4
Sergey <dj.flyagin@gmail.com>
2022-07-03 22:45:41 +0300

    Example_012

---
+++ Example_012/Program.cs
@@ -0,0 +1,25 @@
+﻿// Методы которые не возвращают и не принимают
+//1 вид
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+Method1(); //вызов метода
+
+//2 вид
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg: "Текст", count: 4);
