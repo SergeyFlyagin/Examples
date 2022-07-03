a01f6d0f356537b03ddf7d96ff8507cbc56f191b
Sergey <dj.flyagin@gmail.com>
2022-07-03 21:26:03 +0300

    Example_010

---
+++ Example_010/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] array = { 1, 12, 3, 5, 86, 19, 51, 25, 86 };
+int n = array.Length;
+int find = 86;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
