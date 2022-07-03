97cbf47ada967b2c29911adf5fa7a962c0797a69
Sergey <dj.flyagin@gmail.com>
2022-07-03 22:45:58 +0300

    Example_013

---
+++ Example_013/Program.cs
@@ -0,0 +1,25 @@
+﻿// Метод который возвращает но не принимает
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+//Методы которые что-то принимают
+// и что-то возвращают
+
+string Method4(int count, string text)
+{
+    int i=0;
+    string result=String.Empty;
+    while(i<count)
+    {
+        result=result+text;
+        i++;
+    }
+    return result;
+}
+string res= Method4(10, "asdf");
+Console.WriteLine(res);
