039a954d669b2d6247cb164681161b5b0eff5198
Sergey <dj.flyagin@gmail.com>
2022-07-03 18:08:37 +0300

    Example_004

---
+++ Example_004/Program.cs
@@ -0,0 +1,8 @@
+﻿// Сумма случайных чисел
+
+int numA = new Random().Next(1,10);
+Console.WriteLine(numA);
+int numB = new Random().Next(1,10);
+Console.WriteLine(numA);
+int result = numA + numB;
+Console.WriteLine(result);
