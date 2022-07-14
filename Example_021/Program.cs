ad3cdfe59b11579d33b3a281eb00956dee10c0a4
Sergey <dj.flyagin@gmail.com>
2022-07-14 16:06:08 +0300

    Example_021

---
+++ Example_021/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}!={Factorial(i)}");
+}
